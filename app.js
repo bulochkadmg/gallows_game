0817859fb92f2f4cf411b145458ad82100cbfa90
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-07-16 11:49:03 +0300

    create random word

---
+++ app.js
@@ -0,0 +1,19 @@
+// random word
+let words = [
+    'javascript',
+    'monkey',
+    'amazing',
+    'pancake',
+    'guide',
+    'cat',
+    'banana',
+    'dog',
+    'house',
+    'sea',
+    'found',
+    'member',
+    'splash',
+    'random',
+];
+
+let word = words[Math.floor(Math.random() * words.length)];
