cdfcb91eac9a367dc84e6a7f6b3db86d6c3bd708
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-07-16 12:10:49 +0300

    game cicle create

--- app.js
+++ app.js
@@ -13,16 +13,34 @@ let words = [
     'found',
     'member',
     'splash',
+    'frog',
     'random',
 ];
 
 let word = words[Math.floor(Math.random() * words.length)];
 
 // Answer array
-
 let answerArray = [];
 for(let i = 0; i < word.length; i++) {
     answerArray[i] = '_';
 }
 
-var remainingLetters = word.length;
+let remainingLetters = word.length;
+
+// Game cicle
+while(remainingLetters > 0) {
+    alert(answerArray.join(' '));
+    let guess = prompt('Guess a letter, or click Cancel to stop playing.');
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert('Please enter a single letter');
+    } else {
+        // Update the game state with the guess
+    }
+    // Game code goes here
+    // Show the player their progress
+    // Take input from the player
+    // Update answerArray and remainingLetters
+    // for every correct guess
+}
